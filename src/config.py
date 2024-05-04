6f65b55ea1047771376d900edc3ad60d495cb2b4
Simon Renhult <simon.renhult@hotmail.se>
2024-05-04 19:08:31 +0200

    Allow gif and webp on the tv-man

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", os.path.join(curdir, "static", "pr"))
     ALLOWED_EXTENSIONS = set(
-        os.getenv("ALLOWED_EXTENSIONS", "png,jpg,jpeg,mp4").split(",")
+        os.getenv("ALLOWED_EXTENSIONS", "png,jpg,jpeg,mp4,gif,webp").split(",")
     )
     # Time for each PR in seconds
     PR_TIME = int(os.getenv("PR_TIME", 30))
