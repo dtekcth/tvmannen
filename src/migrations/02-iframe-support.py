664c2836f4ca88a7c23d3b1f36c469ebb02a33ac
pingu <nor@acorneroftheweb.com>
2024-12-10 14:09:56 +0100

    Remove iframe migration

--- src/migrations/02-iframe-support.py
+++
@@ -1,8 +0,0 @@
-from tv import app, db
-from sqlalchemy import text
-
-def upgrade():
-    with app.app_context():
-
-        db.session.execute(text(f"ALTER TABLE pr ADD COLUMN is_iframe BOOLEAN DEFAULT FALSE"))
-        db.session.commit()
